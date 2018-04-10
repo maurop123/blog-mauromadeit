c00e9c613a508df20d2d83dcf00866d122da24bf
mauro <maurop123@gmail.com>
2018-04-10 17:44:19 -0400

    fix up the homepage

---
+++ src/views/home-content.js
@@ -0,0 +1,66 @@
+export default {
+  writing: [{
+    title: 'How Vue-Cli 3 Will Shape Our Future',
+    img: {
+      src: 'https://cdn-images-1.medium.com/max/600/1*asPu5VlOK4iQlNcvqD7w6Q.gif',
+      classes: ['contain'],
+    },
+    link: {
+      url: 'https://medium.com/vuetify/how-vue-cli-3-will-shape-our-future-eb7c01f4a241',
+      source: 'medium',
+    },
+  }, {
+    title: 'Passing methods as props in Vue.js',
+    img: {
+      src: 'https://cdn-images-1.medium.com/max/1024/1*fTV_1UOGZf60Oo-zC7O66w.jpeg',
+    },
+    link: {
+      url: 'https://medium.com/front-end-hacking/passing-methods-as-props-in-vue-js-d65805bccee',
+      source: 'medium',
+    },
+  }, {
+    title: 'Vue’s new and improved prop.sync',
+    img: {
+      src: 'https://cdn-images-1.medium.com/max/599/1*EeFpBMlA7kPCKjfb6sMY9w.png',
+    },
+    link: {
+      url: 'https://medium.com/front-end-hacking/vues-v-model-directive-vs-sync-modifier-d1f83957c57c',
+      source: 'medium',
+    },
+  }, {
+    title: 'Keeping props DRY in Vue.js',
+    img: {
+      src: 'https://cdn-images-1.medium.com/max/632/1*SLrxuR7nE37KfEmee2YXAQ.jpeg',
+    },
+    link: {
+      url: 'https://medium.com/front-end-hacking/keeping-props-dry-in-vue-js-9d8294fa708f',
+      source: 'medium',
+    },
+  }, {
+    title: 'Avoid 80% of Git merge conflicts',
+    img: {
+      src: 'https://media.giphy.com/media/bEjLIhTDqJRaE/giphy.gif',
+    },
+    link: {
+      url: 'https://medium.com/front-end-hacking/avoid-80-of-merge-conflicts-with-git-rebase-b5d755a082a6',
+      source: 'medium',
+    },
+  }],
+  speaking: [{
+    title: 'Vuenos Aires',
+    img: {
+      src: '/static/vuenosaires_animated.svg',
+      classes: ['animate', 'contain'],
+    },
+  }, {
+    title: 'MiamiJS',
+    img: {
+      src: '/static/miamijs_logo.jpeg',
+    },
+  }, {
+    title: 'FL Vocational Institute',
+    img: {
+      src: '/static/fvi_logo.jpg',
+    },
+  }],
+}
